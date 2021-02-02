b7e9cdd7e405786252981597e9c62206caab89be
kirill grinash <kirillgrinash@gmail.com>
2021-02-02 09:47:39 -0600

    added problem 2 for JS and Ruby

--- practice.js
+++ practice.js
@@ -1,10 +1,20 @@
 //////////Create an if statement that uses elsif and else with valid syntax from memory//////////
 
 //1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-var x = 10;
-if (x === 10) {
-  console.log(0);
-} else {
-  console.log(-1);
-}
+// var x = 10;
+// if (x === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
 
+// 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+// var x = 5;
+// if (x < 10) {
+//   console.log(-1);
+// } else if (x > 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
--- practice.rb
+++ practice.rb
@@ -1,9 +1,20 @@
 #//////////Create an if statement that uses elsif and else with valid syntax from memory//////////
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-x = 25
-if x == 10
-  p 0
-else
+# x = 25
+# if x == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, 
+# prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+x = 5
+if x < 10
   p -1
+elsif x > 10
+  p 1
+else
+  p 0
 end
