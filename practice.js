cc8702770785a4de4f2548ecd2624dbb9d683961
kirill grinash <kirillgrinash@gmail.com>
2021-02-01 23:26:20 -0600

    finished problem 1 in JS for if statements

---
+++ practice.js
@@ -0,0 +1,10 @@
+//////////Create an if statement that uses elsif and else with valid syntax from memory//////////
+
+//1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+var x = 10;
+if (x === 10) {
+  console.log(0);
+} else {
+  console.log(-1);
+}
+
---
+++ practice.rb
@@ -0,0 +1,9 @@
+#//////////Create an if statement that uses elsif and else with valid syntax from memory//////////
+
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+x = 25
+if x == 10
+  p 0
+else
+  p -1
+end
