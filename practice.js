bacd387faa28b18a6fb32e31d56e27124bedc99c
kirill grinash <kirillgrinash@gmail.com>
2021-03-27 21:22:24 -0500

    added JS/RUBY practice

--- practice.js
+++ practice.js
@@ -36,8 +36,28 @@
 //   console.log(-1);
 // }
 
+// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 
+// if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+// var x = 11;
+// if (x < 10) {
+//   console.log(9);
+// } else if (x < 20) {
+//   console.log(19);
+// } else if (x < 30) {
+//   console.log(29);
+// } else {
+//   console.log(-1);
+// }
 
-
+// # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10,
+// and prints -100 otherwise.
+// var x = 11;
+// var y = -5;
+// if (x > 10 && y > 10) {
+//   console.log(100);
+// } else {
+//   console.log(-100);
+// }
 
 
 
